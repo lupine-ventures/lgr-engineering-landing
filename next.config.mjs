f4e41a224a863a90635d3e586520e42aebc5717a
lupine-ventures <ramon@usekerf.com>
2026-04-21 20:03:48 -0500

    Add Tap Plan Austin redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.tapplanaustin.com",
+          },
+        ],
+        destination: "https://lrgengineering.com/tap-plans",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
